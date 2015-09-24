8a4a10182560b47d99eabe7239d7efd9991dd05b
elderfd <james_elderfield@hotmail.com>
2015-09-24 18:13:21 +0100

    Tried to change footer

--- wp-content/themes/omega-child/functions.php
+++ wp-content/themes/omega-child/functions.php
@@ -4,4 +4,15 @@
         wp_enqueue_style("parent-style", get_template_directory_uri()."/style.css");
         wp_enqueue_style("parent-style", get_template_directory_uri()."/rtl.css");
     }
+
+    // Change the default footer
+    add_action("wp_loaded", "changeFooter")
+    function changeFooter() {
+        remove_filter( 'omega_footer_insert', 'omega_default_footer_insert' );
+        add_filter( 'omega_footer_insert', 'newFooter' );
+
+        function newFooter( $settings ) {
+            return '<p class="copyright">' . __( 'Copyright &#169; ', 'omega' ) . date_i18n( 'Y' ) . ' ' . get_bloginfo( 'name' ) . '.</p>';
+        }
+    }
 ?>
